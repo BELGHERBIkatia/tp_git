629ee245e25ea041a0e2c49e50b5d68fb436fe68
BELGHERBIkatia <ik_belgherbi@esi.dz>
2022-01-21 11:37:54 +0100

    connexion

---
+++ src/main/java/com/telly/controllers/UserController.java
@@ -0,0 +1,39 @@
+package com.telly.controllers;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class UserController {
+
+
+
+
+	@RequestMapping("/login")
+	public String showLogin() {
+		return "login";
+	}
+	
+
+	
+	
+
+
+
+
+
+	
+
+	
+
+}
+
+
+
