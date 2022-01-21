796ca16e70116c6459661102f386f5975cc4c727
BELGHERBIkatia <ik_belgherbi@esi.dz>
2022-01-21 11:40:25 +0100

    deconnexion

--- src/main/java/com/telly/controllers/UserController.java
+++ src/main/java/com/telly/controllers/UserController.java
@@ -20,8 +20,11 @@ public class UserController {
 	public String showLogin() {
 		return "login";
 	}
-	
 
+	@RequestMapping("/loggedout")
+	public String showLogout() {
+		return "loggedout";
+	}
 	
 	
 
